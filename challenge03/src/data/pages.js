6d1a2bcffd9e82cd9f1ecad8f2d56f67ed8b1cc2
jorgecorrea-uao <jorge.correa@uao.edu.co>
2026-09-09 16:26:51 -0500

    agregar datos mockeados de canciones y paginas

---
+++ challenge03/src/data/pages.js
@@ -0,0 +1,8 @@
+const pages = [
+  { id: 1, title: "Prueba 1", url: "https://Prueba1.com" },
+  { id: 2, title: "Prueba 2", url: "https://Prueba2.com" },
+  { id: 3, title: "Prueba 3", url: "https://Prueba3.com" },
+  { id: 4, title: "Prueba 4", url: "https://Prueba4.com" },
+];
+
+export default pages;
---
+++ challenge03/src/data/songs.js
@@ -0,0 +1,9 @@
+const songs = [
+  { id: 1, title: "Prueba 1", artist: "Artist 1", duration: "5:55" },
+  { id: 2, title: "Prueba 2", artist: "Artist 2", duration: "6:30" },
+  { id: 3, title: "Prueba 3", artist: "Artist 3", duration: "5:01" },
+  { id: 4, title: "Prueba 4", artist: "Artist 4", duration: "4:54" },
+  { id: 5, title: "Prueba 5", artist: "Artist 5", duration: "3:03" },
+];
+
+export default songs;
